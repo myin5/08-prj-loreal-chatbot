f6b755300f3cef5e37a297dbf84bd9a53b636dbc
Mingyao Yin <myin5@gmu.edu>
2025-10-30 01:56:48 +0000

    Remove model from fetchReply request body to streamline message handling

--- script.js
+++ script.js
@@ -79,8 +79,7 @@ async function fetchReply(messages) {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      model: "gpt-4o",
-      messages
+      messages 
     }),
   });
 
